18f95cb0b08382a4d356e5a660b67f230638e6ff
mallowlabs <mallowlabs@gmail.com>
2014-06-04 22:29:19 +0900

    Added: heroku issues command

--- README.md
+++ README.md
@@ -4,5 +4,8 @@ An ruboty handler to see Heroku Status.
 ## Usage
 ```
 @ruboty heroku status - See Heroku Status
+@ruboty heroku issues - See Heroku Issues
 ```
 
+## Credit
+I refered [Hubot script by juno](https://github.com/github/hubot-scripts/blob/master/src/scripts/heroku-status.coffee).
--- lib/ruboty/handlers/heroku_status.rb
+++ lib/ruboty/handlers/heroku_status.rb
@@ -5,15 +5,31 @@ module Ruboty
   module Handlers
     class HerokuStatus < Base
       on /heroku status/, name: "heroku", description: "See Heroku Status"
+      on /heroku issues/, name: "issues", description: "See Heroku Issues"
 
       def heroku(message)
-        content = open("https://status.heroku.com/api/v3/current-status").read
-        json = JSON.parse(content)
+        json = fetch("https://status.heroku.com/api/v3/current-status")
         body = <<MESSAGE
 Production: #{json["status"]["Production"]}
 Development: #{json["status"]["Development"]}
 MESSAGE
         message.reply(body)
+      end
+
+      def issues(message)
+        json = fetch("https://status.heroku.com/api/v3/issues?limit=5")
+        body = json.map { |issue|
+          resolved = issue['resolved'] ? "(resolved)" : "(unresolved)"
+          "#{issue['id']}: #{issue['title']} #{resolved}"
+        }.join("\n")
+        message.reply(body)
+      end
+
+      private
+
+      def fetch(url)
+        content = open(url).read
+        json = JSON.parse(content)
       end
 
     end
